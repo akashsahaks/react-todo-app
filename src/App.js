871b027b26ffda7656f15aefa1a06264255492d3
akash <akashsah.aks@gmail.com>
2022-12-20 08:38:16 +0530

    Project is working smoothly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Todos from "./components/Todos";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container" style={{ width: "60%" }}>
+        <div className="App">
+          <Todos />
+        </div>
+      </div>
+    </>
   );
 }
 
